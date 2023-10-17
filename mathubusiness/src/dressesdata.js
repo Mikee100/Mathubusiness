3147110df990b9284152d2a45298d55f7ac23acc
Mike <mikekariuki10028@gmail.com>
2023-10-17 21:53:08 +0300

    changed name

--- mathubusiness/src/dressesdata.js
+++ mathubusiness/src/dressesdata.js
@@ -2,60 +2,60 @@ const dressesdata = [
     {
       id: 0,
       image: "/images/dress1.jfif",
-      title: "Chanel Bag",
+      title: "Wedding dress",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Wedding dress from Spain made of pure cotton",
       price: 2500,
     },
     {
       id: 1,
       image: "/images/dress2.jfif",
-      title: "Chanel Bag",
+      title: "Wedding dress",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Wedding dress from Spain made of pure cotton",
   
       price: 10000,
     },
     {
       id: 2,
       image: "/images/dress3.jfif",
-      title: "Chanel Bag",
+      title: "Wedding dress",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Wedding dress from Spain made of pure cotton",
   
       price: 9500,
     },
     {
       id: 3,
       image: "/images/dress4.jfif",
-      title: "Chanel Bag",
+      title: "Wedding dress",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Wedding dress from Spain made of pure cotton",
       price: 6500,
     },
     {
       id: 4,
       image: "/images/dress5.jfif",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
-      title: "Chanel Bag",
+        "This is a very good looking Wedding dressfrom Spain made of pure cotton",
+      title: "Wedding dress",
       price: 5500,
     },
     {
       id: 5,
       image: "/images/dress6.jfif",
-      title: "Chanel Bag",
+      title: "Wedding dress",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Wedding dress from Spain made of pure cotton",
   
       price: 4500,
     },
     {
       id: 6,
       image: "/images/dress7.jfif",
-      title: "Chanel Bag",
+      title: "Wedding dress",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Wedding dress from Spain made of pure cotton",
   
       price: 10500,
     },
@@ -63,9 +63,9 @@ const dressesdata = [
     {
       id: 7,
       image: "/images/dress8.jfif",
-      title: "Chanel Bag",
+      title: "Wedding dress",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Wedding dress from Spain made of pure cotton",
   
       price: 12500,
     },
--- mathubusiness/src/shoes.js
+++ mathubusiness/src/shoes.js
@@ -2,51 +2,51 @@ const shoes = [
     {
       id: 0,
       image: "/images/shoe1.jfif",
-      title: "Chanel Bag",
+      title: "Kensie Shoe",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
       price: 2500,
     },
     {
       id: 1,
       image: "/images/shoe2.jfif",
-      title: "Chanel Bag",
+      title: "Kensie Shoe",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
   
       price: 10000,
     },
     {
       id: 2,
       image: "/images/shoe3.jfif",
-      title: "Chanel Bag",
+      title: "Kensie Shoe",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
   
       price: 9500,
     },
     {
       id: 3,
       image: "/images/shoe4.jfif",
-      title: "Chanel Bag",
+      title: "Kensie Shoe",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
       price: 6500,
     },
     {
       id: 4,
       image: "/images/shoe5.jfif",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
-      title: "Chanel Bag",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
+      title: "Kensie Shoe",
       price: 5500,
     },
     {
       id: 5,
       image: "/images/shoe6.jfif",
-      title: "Chanel Bag",
+      title: "Kensie Shoe",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
   
       price: 4500,
     },
@@ -55,7 +55,7 @@ const shoes = [
       image: "/images/shoe7.jfif",
       title: "Chanel Bag",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
   
       price: 10500,
     },
@@ -63,9 +63,9 @@ const shoes = [
     {
       id: 7,
       image: "/images/shoe8.jfif",
-      title: "Chanel Bag",
+      title: "Kensie Shoe",
       about:
-        "This is a very good looking Chanel bag from Spain made of pure cotton",
+        "This is a very good looking Kensie Shoe from Spain made of pure cotton",
   
       price: 12500,
     },
