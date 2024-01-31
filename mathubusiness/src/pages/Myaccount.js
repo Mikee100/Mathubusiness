b1969333d63776e3a51efe6a52dfbbf6900288cf
Mikee100 <99830445+Mikee100@users.noreply.github.com>
2024-01-31 23:23:09 +0300

    worked on myaccount and firebase

--- mathubusiness/src/pages/Myaccount.js
+++ mathubusiness/src/pages/Myaccount.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import "./home.css";
+import { collection, getDocs } from "firebase/firestore";
+import {db} from "../Firebase"
 
-export default function myAccount() {
+export default function Myaccount() {
+ 
+
+  const [todos, setTodos] = useState([]);
+
+  const fetchPost = async () => {
+    await getDocs(collection(db, "Account Information"))
+      .then((querySnapshot)=>{
+          const newData = querySnapshot.docs
+              .map((doc) => ({...doc.data(), id:doc.id }));
+          setTodos(newData);
+          console.log(todos, newData);
+      })
+  }
+  useEffect(()=>{
+    fetchPost();
+  }, [])
   return (
-    <div>myAccount</div>
+    <div className='mymostdata' >{
+      todos.map((todo,i)=>(
+          <p key={i}>
+              {todo.todo}
+          </p>
+      ))
+    }
+    <h1>Waweru</h1>
+    </div>
   )
 }
