4a718c2e74d35a075163ff6a5379a508691a6553
Mikee100 <99830445+Mikee100@users.noreply.github.com>
2024-03-09 20:39:14 +0300

    working on firebase

--- mathubusiness/src/pages/Mainpage.js
+++ mathubusiness/src/pages/Mainpage.js
@@ -15,8 +15,12 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function Mainpage({productItems,   handleAddProductDetails,productDresses, productShoes,fashionwomenboot,productshoessandals}) {
   const [myproducts, MysetProducts] = useState([]);
+  const [products_shoes, setProducts_shoes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
 
   useEffect(() => {
+
     const fetchProducts = async () => {
       const querySnapshot = await getDocs(collection(db, "Product Information"));
       const products = querySnapshot.docs.map(doc => ({
@@ -25,9 +29,32 @@ export default function Mainpage({productItems,   handleAddProductDetails,produc
       }));
       console.log(products);
       MysetProducts(products);
+      setIsLoading(false);
     };
     fetchProducts();
   }, []);
+
+
+  useEffect(() => {
+
+    const fetchProducts = async () => {
+      const querySnapshot = await getDocs(collection(db, "Shoes"));
+      const products = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      console.log(products);
+      setProducts_shoes(products);
+      setIsLoading(false);
+    };
+    fetchProducts();
+  }, []);
+
+
+  
+  if (isLoading) {
+    return <div className="loading-message">Loading products...</div>;
+  }
 
 
   return (
@@ -39,10 +66,13 @@ export default function Mainpage({productItems,   handleAddProductDetails,produc
 
            
            <div className="products"    >
-            <div className="prdt_category" >
+           <div className="prdt_category" >
                  <h3 className="h3_title" >Bags</h3>
                  <a href="./bags" className="a_bags_see_all" >See all<MdKeyboardArrowRight className="right_see_all" /></a>
             </div>
+      
+     
+           
         
 {myproducts.map((product,index) => (
   
@@ -50,6 +80,12 @@ export default function Mainpage({productItems,   handleAddProductDetails,produc
   <Link key={product.id}
   // Moving to the product page
   to={`/productdetails?name=${product.title}?id=${product.id}`}  >
+            {isLoading ?  (
+        <div className="loading">
+          <p>Loading products...</p>
+          {/* Add a loading spinner if you want */}
+        </div>
+      ) : (
      <div className="product_firstmainpage"  
   onClick={() => handleAddProductDetails(product)} key={product.id}>
  key={index}
@@ -68,27 +104,14 @@ export default function Mainpage({productItems,   handleAddProductDetails,produc
      KSh {product.price} 
     </span>
 
-
-{/** 
-    <div className="buttons"> 
-   <Link to="/productdetails" > <button className="btn" onClick={() => handleAddProductDetails(product)} > details </button></Link> 
-   <button className="btn"  onClick={() => handleAddProduct(product)} > 
-    Add to Cart 
-   
-    </button>
-
-
-   </div>
-*/}
-
   </div>
-  
+  ) }
 
 
 </Link> 
 
-     ))}
-
+     ))};
+  
 
 </div>
 
@@ -98,7 +121,7 @@ export default function Mainpage({productItems,   handleAddProductDetails,produc
                  <h3>Shoes</h3>
                  <a href="./shoes" className="a_bags_see_all" >See all<MdKeyboardArrowRight className="right_see_all" /></a>
             </div>
-{productShoes.map((product) => (
+{products_shoes.map((product) => (
   
 
   <Link key={product.id}
--- mathubusiness/src/pages/Sellers/Sellers.js
+++ mathubusiness/src/pages/Sellers/Sellers.js
@@ -1,9 +1,10 @@
-import React,{useState, useEffect} from 'react'
-import "../Sellers/sellers.css"
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { db} from '../../Firebase';
+import React, { useState, useEffect } from 'react';
+import "../Sellers/sellers.css";
 
-import { addDoc, collection, getDocs  } from "firebase/firestore"; 
+import { db } from '../../Firebase';
+
+import { addDoc, collection, getDocs } from "firebase/firestore";
+
 export default function Sellers() {
 
   const [title, setTitle] = useState("");
@@ -11,101 +12,75 @@ export default function Sellers() {
   const [about, setAbout] = useState("");
   const [image, setImage] = useState("");
   const [products, setProducts] = useState([]);
-  const handleImageChange = async (e,downloadURL) => {
-    const image = e.target.files[0];
-    const storage = getStorage();
-    const storageRef = ref(storage, 'images/' + image.name);
-    const uploadTask = uploadBytesResumable(storageRef, image);
-  
-    uploadTask.on('state_changed',
-      (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-      },
-      (error) => {
-        console.error(error);
-      },
-      async () => {
-        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-        console.log('File available at', downloadURL);
-        // Store the downloadURL in your Firestore
-        await addDoc(collection(db, "Product Information"), {
-          title: title,
-          price: price,
-          about: about,
-          image: downloadURL,
-        });
-        setTitle("");
-        setPrice("");
-        setAbout("");
-        setImage("");
-      }
-    );
+
+ 
+
+  const handleCategorySubmit = (e) => {
+    const category = e.target.value;
+    handleSubmit(category, e);
   };
 
-  
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (category, e) => {
     e.preventDefault();
 
-    
     try {
-      const docRef = await addDoc(collection(db, "Product Information"), {
+      const docRef = await addDoc(collection(db, category), {
         title: title,
         price: price,
         about: about,
         image: image,
-       
-
-     
       });
-    
-      alert("Registration was successfull ", docRef.id);
+
+      alert("Registration was successful ", docRef.id);
     } catch (e) {
       alert("Error adding document: ", e);
     }
-    
-   
-
+  };
   
 
-};
-useEffect(() => {
-  const fetchProducts = async () => {
-    const querySnapshot = await getDocs(collection(db, "Product Information"));
-    const products = querySnapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-    console.log(products);
-    setProducts(products);
-  };
-  fetchProducts();
-}, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const querySnapshot = await getDocs(collection(db, "Product Information"));
+      const products = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      console.log(products);
+      setProducts(products);
+    };
+    fetchProducts();
+  }, []);
+
+ 
 
   return (
-    <div className='seller_container' >
-      <form className='seller_form' onSubmit={handleSubmit} >
-        <input type="text" className='product_title' placeholder='Product Title' value={title} onChange={(e) => setTitle(e.target.value) }  />
-        <input type="text" className='product_price' placeholder='Product Price' value={price} onChange={(e) => setPrice(e.target.value) }  />
-        <input type="text" className='product_about' placeholder='Product About' value={about} onChange={(e) => setAbout(e.target.value) }  />
-        <input type='text' className='product_seller_image' placeholder='Product image'  value={image} onChange={(e) => setImage(e.target.value) }/>
+    <div className='seller_container'>
+      <form className='seller_form' onSubmit={handleSubmit}>
+        <input type="text" className='product_title' placeholder='Product Title' value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" className='product_price' placeholder='Product Price' value={price} onChange={(e) => setPrice(e.target.value)} />
+        <input type="text" className='product_about' placeholder='Product About' value={about} onChange={(e) => setAbout(e.target.value)} />
+        <input type='text' className='product_seller_image' placeholder='Product image' value={image} onChange={(e) => setImage(e.target.value)} />
 
-        <input type="file"  onChange={handleImageChange} />
-        <button className='seller_btn' >Send Products</button>
+        <select name="category" onChange={handleCategorySubmit} className='mycategory' >
+          <option value="">Select Category</option>
+          <option value="Shoes">Shoes</option>
+          <option value="Bags">Bags</option>
+          <option value="dresses">Dresses</option>
+        </select>
+
+        <button className='seller_btn'>Send Products</button>
       </form>
-      
+
       <div className='product_list'>
         {products.map(product => (
           <div className='product' key={product.id}>
-            <p className="product-title" >{product.title}</p>
-            <p  >{product.about}</p>
-            <p className="product-price" >{product.price}</p>
-            <img  className='product-image' src={product.image} alt={product.image} />
-
-</div>
-            ))}
-            </div>
+            <p className="product-title">{product.title}</p>
+            <p>{product.about}</p>
+            <p className="product-price">{product.price}</p>
+            <img className='product-image' src={product.image} alt={product.image} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
